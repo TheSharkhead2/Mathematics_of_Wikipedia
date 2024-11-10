a7fc0932c93c6cec0385b5289ca3cc37f0b63eff
Theo Rode <theorodester@gmail.com>
2024-11-10 21:20:35 +0000

    category pulling !

--- src/WikipediaDownload/WikipediaDownload.jl
+++ src/WikipediaDownload/WikipediaDownload.jl
@@ -1,3 +1,6 @@
 module WikipediaDownload 
 
+include("recursive_category.jl")
+export pull_wikipedia
+
 end # module WikipediaDownload
--- src/WikipediaStructure.jl
+++ src/WikipediaStructure.jl
@@ -3,5 +3,6 @@ module WikipediaStructure
 include("WikipediaDownload/WikipediaDownload.jl")
 
 using .WikipediaDownload
+export pull_wikipedia
 
 end # module WikipediaStructure
