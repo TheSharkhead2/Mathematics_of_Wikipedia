53ac5f17eba2d7131d278ff82a7452ea9568c4b0
Theo Rode <theorodester@gmail.com>
2024-11-25 13:15:16 -0800

    gitignore

--- src/WikipediaStructure.jl
+++ src/WikipediaStructure.jl
@@ -6,4 +6,10 @@ using .WikipediaDownload
 export pull_wikipedia
 export pull_page_network, pull_page_network_from_checkpoint
 
+include("Categories/Categories.jl")
+
+using .Categories
+export mathematics_fields_categories 
+
 end # module WikipediaStructure
+
